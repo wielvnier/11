e721ccbe883b06999eebf6a080f814db7c0b23b0
wielvnier <filipwielanier@gmail.com>
2017-06-12 20:13:30 +0200

    2_commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+}
+
+var iPhone6S = new Phone("Apple", 2250, "silver");
+
+iPhone6S.printInfo();
